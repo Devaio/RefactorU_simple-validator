609504e026ff49cc008bfac83c3face4fa4cbe77
Robert Camp <robert.camp@outlook.com>
2013-09-25 14:02:52 -0600

    began fixing some issues

--- js/myScript.js
+++ js/myScript.js
@@ -1,12 +1,12 @@
 // Phone Number Prompt
 var phoneNum = prompt("What is your phone number? Please enter with -'s in the appropriate spots. XXX-XXX-XXXX  Don't worry, your phone number will NEVER be used for spam.");
-if(phoneNum[3] === "-" && phoneNum[7] === "-" && phoneNum.length > 0) {
+if(phoneNum[3] === "-" && phoneNum[7] === "-" && phoneNum.length === 12) {
  	alert("Thanks!  The number we have for you is " + phoneNum + ".");
 }
 else {
 	alert("Check your format: XXX-XXX-XXXX Let's try again!");
-	var phoneNum = prompt("What is your phone number? Please enter with -'s in the appropriate spots. XXX-XXX-XXXX  Don't worry, your phone number will NEVER be used for spam.");
-	if(phoneNum[3] === "-" && phoneNum[7] === "-" && phoneNum.length > 0){
+	 phoneNum = prompt("What is your phone number? Please enter with -'s in the appropriate spots. XXX-XXX-XXXX  Don't worry, your phone number will NEVER be used for spam.");
+	if(phoneNum[3] === "-" && phoneNum[7] === "-" && phoneNum.length === 12){
 		alert("Thanks!  The number we have for you is " + phoneNum + ".");
 }
 }
@@ -18,7 +18,7 @@ if(birthDate[2] === "/" && birthDate[5] === "/" && birthDate.length === 8) {
 }
 else {
 	alert("Look's like you forgot something! Let's try again!");
-	var birthDate = prompt("Ok! How about your birth date? Please use XX/XX/XX format!");
+	 birthDate = prompt("Ok! How about your birth date? Please use XX/XX/XX format!");
 	if(birthDate[2] === "/" && birthDate[5] === "/" && birthDate.length === 8) {
 		alert("Awesome!  Looks like you were born on " + birthDate + "!");
 }
@@ -31,7 +31,7 @@ if (stateAbbr === stateAbbr.toUpperCase() && stateAbbr.length === 2) {
 }
 else {
 	alert("Look's like you may need to re-enter that.  Remember, please use capital letters on your 2-digit state abbreviation!")
-	var stateAbbr = prompt("What's the abbreviation for the state you live in? Please use all capital letters!");
+	 stateAbbr = prompt("What's the abbreviation for the state you live in? Please use all capital letters!");
 	if (stateAbbr === stateAbbr.toUpperCase() && stateAbbr.length === 2) {
 	alert("Thanks! You live in " + stateAbbr + "!");
 }
@@ -45,8 +45,8 @@ if ((married === married.toUpperCase() || married === married.toLowerCase() || m
 }
 else {
 	alert("Oops! Remember to answer with a simple yes or no!");
-	var married = prompt("Are you married? Yes/No");
-	if ((married === married.toUpperCase() || married === married.toLowerCase() || married === married.charAt(0).toUpperCase()) && married.length > 0) {
+	 married = prompt("Are you married? Yes/No");
+	if ((married === married.toUpperCase() || married === married.toLowerCase() || married) && married.length > 0) {
 	alert("Are you married? " + married.charAt(0).toUpperCase() + marriedReport.toLowerCase() + ".")
 }
 }
